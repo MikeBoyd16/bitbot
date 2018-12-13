d3f4836dc51ee5c3bd4dbd7dfb3eebdf769b9bb4
MikeBoyd16 <mikeboyd165@gmail.com>
2018-12-13 09:31:29 -0800

    Add save, load, and reset functionality.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,82 @@
+/*
+ *	Author: Michael Boyd
+ *	Date: 12/12/18
+ *	File: main.js
+ */
 var bits = 0;
-function bitClick(number) {
-    bits = bits + number;
-    document.getElementById("bits").innerHTML = bits;
-};
-
 var bots = 0;
-function buyBot() {
-    var botCost = Math.floor(10 * Math.pow(1.1, bots));
-    if(bits >= botCost){
-        bots = bots + 1;
-    	bits = bits - botCost;
-        document.getElementById('bots').innerHTML = bots;
-        document.getElementById('bits').innerHTML = bits;
+var botCost = 10;
+
+/* 
+ * Saves the game data 
+ */
+function save() {
+    var save = {
+        bits: bits,
+        bots: bots
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1, bots));
-    document.getElementById('botCost').innerHTML = nextCost;
+    localStorage.setItem("save", JSON.stringify(save));
 };
 
+/* 
+ * Loads an existing save into the current game 
+ */
+function load() {
+    var savegame = JSON.parse(localStorage.getItem("save"));
+
+    // Local storage returns null if the item doesn't exist
+    if (savegame !== null) {
+        if (typeof savegame.bits !== "undefined") bits = savegame.bits;
+        if (typeof savegame.bots !== "undefined") bots = savegame.bots;
+        botCost = Math.floor(10 * Math.pow(1.1, bots));
+        refreshDisplayedData();
+    };
+};
+
+/* 
+ * Resets all game data and removes the save, if it exists 
+ */
+function reset() {
+    bits = 0;
+    bots = 0;
+    botCost = 10;
+    localStorage.removeItem("save");
+    refreshDisplayedData();
+};
+
+/*
+ * Adds bits to the current bit amount based on the number of mined bits 
+ */
+function mineBits(number) {
+    bits = bits + number;
+    refreshDisplayedData();
+};
+
+/* 
+ * Adds a bot to the current bot amount and subtracts its cost from the current bit amount 
+ */
+function buyBot() {
+    if (bits >= botCost) {
+        bits = bits - botCost;
+        bots++;
+        botCost = Math.floor(10 * Math.pow(1.1, bots));
+        refreshDisplayedData();
+    };
+};
+
+/*
+ * Refreshes the displayed game data
+ */
+function refreshDisplayedData() {
+    document.getElementById('bits').innerHTML = bits;
+    document.getElementById('bots').innerHTML = bots;
+    document.getElementById('botCost').innerHTML = botCost;
+};
+
+// Load the save, if it exists
+load();
+
+// Bots mine bits every second
 window.setInterval(function() {
-	bitClick(bots);
+    mineBits(bots);
 }, 1000);
